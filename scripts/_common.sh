f1a684889bc0c757a06f2777a39a64dc25e2db99
Le Libre Au Quotidien <contact@lelibreauquotidien.fr>
2021-06-09 20:52:25 +0200

    add python dep

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="python3-venv"
 
 #=================================================
 # PERSONAL HELPERS
